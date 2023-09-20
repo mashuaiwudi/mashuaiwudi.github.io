f8737f66f31d8e3cb15055288a11b6d4aa886513
mashuaiwudi <983101136@qq.com>
2023-09-20 19:58:32 +0800

    add a live demo page

---
+++ HAIcollaboration/static/js/HAIcollaboration.js
@@ -0,0 +1,30 @@
+
+
+function GoToDirectDisplay(){
+    var experiment_link = "DirectDisplay.html";
+	window.location.href = experiment_link;
+}
+
+
+function GoToAdaptiveWorkflow(){
+    var experiment_link = "AdaptiveWorkflow.html";
+	window.location.href = experiment_link;
+}
+
+
+function GoToAdaptiveRecommendation(){
+    var experiment_link = "AdaptiveRecommendation.html";
+	window.location.href = experiment_link;
+}
+
+
+function GoToHumanOnly(){
+    var experiment_link = "HumanOnly.html";
+	window.location.href = experiment_link;
+}
+
+
+function GoToAIConfidence(){
+    var experiment_link = "AIConfidence.html";
+	window.location.href = experiment_link;
+}
